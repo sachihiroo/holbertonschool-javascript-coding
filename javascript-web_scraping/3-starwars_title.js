7a7ef8f843e57f50fc89b6d2c028bf83dc929844
sachihiroo <saif3antar@gmail.com>
2024-05-29 17:06:36 +0100

    Star wars movie title

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -6,13 +6,13 @@ const movieId = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
 request(url, (error, response, body) => {
-    if (error || response.statusCode!== 200) {
-      console.error(error);
-      return;
-    }
+	if (error || response.statusCode !== 200) {
+		console.error(error);
+		return;
+	}
 
-    // Parse the JSON response
-    const movieData = JSON.parse(body);
+	// Parse the JSON response
+	const movieData = JSON.parse(body);
 	console.log(movieData.title);
 
 });
