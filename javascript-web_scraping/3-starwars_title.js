a16d4ca92434a1ffdfaaa1d460fcf068e3d31a7b
sachihiroo <saif3antar@gmail.com>
2024-05-29 17:13:30 +0100

    semistandard fixed

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -6,13 +6,12 @@ const movieId = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
 request(url, (error, response, body) => {
-	if (error || response.statusCode !== 200) {
-		console.error(error);
-		return;
-	}
+  if (error || response.statusCode !== 200) {
+    console.error(error);
+    return;
+  }
 
-	// Parse the JSON response
-	const movieData = JSON.parse(body);
-	console.log(movieData.title);
-
+  // Parse the JSON response
+  const movieData = JSON.parse(body);
+  console.log(movieData.title);
 });
